a9d831e7361169453bca998a4a6afa69f640a372
Ignat <ai73a@yandex.ru>
2020-02-03 21:21:08 +0300

    Updates

---
+++ precache-manifest.80be66c0cfab95710e3675f73db8b33c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2271a8d7b1bf02f864b7b9394f67484f",
+    "url": "/students/index.html"
+  },
+  {
+    "revision": "0e203b40e0b332c62f41",
+    "url": "/students/static/css/main.2fb0856e.chunk.css"
+  },
+  {
+    "revision": "d220656eaafb75fa83af",
+    "url": "/students/static/js/2.5d05a9d8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/students/static/js/2.5d05a9d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e203b40e0b332c62f41",
+    "url": "/students/static/js/main.7a50e7c1.chunk.js"
+  },
+  {
+    "revision": "864ded936a204ab27743",
+    "url": "/students/static/js/runtime-main.276f0424.js"
+  }
+]);

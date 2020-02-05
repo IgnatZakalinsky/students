17129796d468ccd09591097087ee3ee5ed27116b
ai73aaa <1qazxcvBG>
2020-02-05 12:00:49 +0300

    refactoring

--- src/neko-2-features/f-6-students/s-1-table/t-1-ui/students/VasilievArtiomAleksandrovich/s-1-name/S4Name.tsx
+++ src/neko-2-features/f-6-students/s-1-table/t-1-ui/students/VasilievArtiomAleksandrovich/s-1-name/S4Name.tsx
@@ -4,22 +4,17 @@ import s from './name.module.css';
 const S4Name = () => {
     const [show, setShow] = useState(true);
 
-    return (
-        <div>
-            {show
-                ? (
-                    <div className={s.name} onMouseOver={() => setShow(false)}>
-                        Васильев Артем Александрович
-                    </div>
-                )
-                : (
-                    <div className={s.text} onMouseOut={() => setShow(true)}>
-                        Пришел в инкубатор бомбить REACT!
-                    </div>
-                )
-            }
-        </div>
-    )
+    return show
+        ? (
+            <div className={s.name} onMouseOver={() => setShow(false)}>
+                Васильев Артем Александрович
+            </div>
+        )
+        : (
+            <div className={s.text} onMouseOut={() => setShow(true)}>
+                Пришел в инкубатор бомбить REACT!
+            </div>
+        )
 };
 
 export default S4Name;

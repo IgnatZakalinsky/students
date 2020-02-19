615c1cd62ed3f8bbc85b3d4452c9dbe41848557a
ai73aaa <1qazxcvBG>
2020-02-19 18:41:57 +0300

    S4state

--- src/neko-2-features/f-6-students/s-1-table/t-1-ui/students/VasilievArtiomAleksandrovich/s-3-state/S4Hello.tsx
+++ src/neko-2-features/f-6-students/s-1-table/t-1-ui/students/VasilievArtiomAleksandrovich/s-3-state/S4Hello.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import styles from './css/Counter.module.css'
 
-
 class S4Hello extends React.Component {
     alertName = React.createRef<HTMLInputElement>();
     state = {counter: 0};
 
     onButtonClick = () => {
-        alert("Привет " + this.alertName.current!.value);
-        this.alertName.current!.value = "";
-        this.setState({
-            counter: this.state.counter + 1
-        });
+        if (this.alertName.current!.value == "") {
+            alert('Введите свое имя')
+        } else {
+            alert("Привет " + this.alertName.current!.value);
+            this.alertName.current!.value = "";
+            this.setState({
+                counter: this.state.counter + 1
+            });
+        }
     };
 
     render = () => {
         return (
             <div className={styles.container}>
-                <span>{this.state.counter}</span>
+                <span className={styles.span}>{this.state.counter}</span>
                 <input ref={this.alertName} type="text"/>
-                <button onClick={this.onButtonClick}>Жми!</button>
+                <button className={styles.button} onClick={this.onButtonClick}>Жми!</button>
             </div>
         );
     }

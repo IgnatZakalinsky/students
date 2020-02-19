a165434602368feada473cd1d8c2b1c215b430ae
ai73aaa <1qazxcvBG>
2020-02-19 14:18:44 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/students/precache-manifest.31c8c12d246981749809cbb88d16d2bf.js"
+  "/students/precache-manifest.10bf96b0042dd7276e6fc117dffb63e3.js"
 );
 
 self.addEventListener('message', (event) => {
